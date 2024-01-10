10f8e83416e11b69c7026e4e797925d00b493e81
Nord.A <mr.nordakov@gmail.com>
2024-01-10 16:38:37 +0300

    seminar1

--- seminars/seminar1/script.js
+++ seminars/seminar1/script.js
@@ -60,7 +60,7 @@ function capitalizeFirstLetter(string) {
   // 1.11 time
   const arr = string.split(" ");
   const arr2 = arr.map((item) => {
-    item[0].toUpperCase() + item.slice(1);
+    return item[0].toUpperCase() + item.slice(1);
   });
   return arr2.join(" ");
 }
@@ -68,3 +68,88 @@ function capitalizeFirstLetter(string) {
 console.log(
   capitalizeFirstLetter("качестве аргумента и возвращает новую строку")
 );
+
+// Задание 3 (Замыкания 20 минут)
+
+// 1. Напишите функцию createCalculator, которая принимает начальное
+// значение и возвращает объект с двумя методами: add и subtract.
+// Метод add должен увеличивать значение на переданное число, а
+// метод subtract должен уменьшать значение на переданное число.
+// Значение должно быть доступно только через методы объекта, а не
+// напрямую.
+
+const createCalculator = (incomingNumber) => {
+  return {
+    add: (arg) => {
+      incomingNumber += arg;
+    },
+    subtract: (arg) => {
+      incomingNumber -= arg;
+    },
+    getNumber: () => {
+      return incomingNumber;
+    },
+  };
+};
+
+let addres = createCalculator(1);
+addres.add(2);
+console.log(addres.getNumber());
+
+// Задание 4 (Лексический контекст 15 минут)
+
+// 1. Напишите функцию createGreeting, которая принимает имя
+// пользователя и возвращает функцию, которая будет выводить
+// приветствие с использованием этого имени.
+// // Пример использования:
+
+function createGreeting(name) {
+  return () => {
+    console.log(`Hello, ${name}`);
+  };
+}
+
+const greeting = createGreeting("John");
+greeting(); // Ожидаемый результат: "Hello, John!"
+
+// Задание 5 (тайминг 15 минут)
+// 1. Задача: Напишите функцию createPasswordChecker, которая
+// принимает допустимую длину пароля в качестве аргумента и
+// возвращает функцию для проверки введенного пароля.
+// Возвращаемая функция должна принимать пароль и возвращать
+// true, если его длина соответствует допустимой, и false в противном
+// случае.
+// Пример использования:
+// const isPasswordValid = createPasswordChecker(8);
+// console.log(isPasswordValid("password")); // Ожидаемый результат:
+// false;
+// console.log(isPasswordValid("secret")); // Ожидаемый результат: true
+
+function createPasswordChecker(passwordLength) {
+  return (password) => {
+    return password.length < passwordLength;
+  };
+}
+
+const isPasswordValid = createPasswordChecker(8);
+console.log(isPasswordValid("password")); // Ожидаемый результат: false
+console.log(isPasswordValid("secret")); // Ожидаемый результат: true
+
+// Задание 6 (Рекурсия 25 минут)
+// 1. Напишите рекурсивную функцию sumDigits, которая принимает
+// положительное целое число в качестве аргумента и возвращает
+// сумму его цифр.
+// // Пример использования:
+// console.log(sumDigits(123)); // Ожидаемый результат: 6 (1 + 2 + 3)
+// console.log(sumDigits(456789)); // Ожидаемый результат: 39 (4 + 5 + 6
+// + 7 + 8 + 9)
+
+function sumDigits(num) {
+  if (num < 10) {
+    return num;
+  }
+  return (num % 10) + sumDigits(Math.trunc(num / 10));
+}
+
+console.log(sumDigits(123)); // Ожидаемый результат: 6 (1 + 2 + 3)
+console.log(sumDigits(456789)); // Ожидаемый результат: 39 (4 + 5 + 6 + 7 + 8 + 9)
