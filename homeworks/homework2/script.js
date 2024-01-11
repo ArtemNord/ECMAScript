af94b99e37ed68f51f666ac91483d4d74711ad09
Naganishe <mr.nordakov@mail.ru>
2024-01-11 23:15:55 +0300

    homework2

---
+++ homeworks/homework2/script.js
@@ -0,0 +1,52 @@
+// Задание 1: ""Управление библиотекой книг""
+
+// Реализуйте класс Book, представляющий книгу, со следующими свойствами и методами:
+
+// Свойство title (название) - строка, название книги.
+// Свойство author (автор) - строка, имя автора книги.
+// Свойство pages (количество страниц) - число, количество страниц в книге.
+// Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).
+
+class Book {
+  constructor(bTitle, bAuthor, bPages) {
+    this.title = bTitle;
+    this.author = bAuthor;
+    this.pages = bPages;
+  }
+
+  displayInfo() {
+    console.log(
+      `Название книги - ${this.title}, автор - ${this.author}, количество страниц - ${this.pages}`
+    );
+  }
+}
+
+const book = new Book("Анна Каренина", "Лев Толстой", 864);
+book.displayInfo();
+
+// Задание 2: ""Управление списком студентов""
+// Реализуйте класс Student, представляющий студента, со следующими свойствами и методами:
+
+// Свойство name (имя) - строка, имя студента.
+// Свойство age (возраст) - число, возраст студента.
+// Свойство grade (класс) - строка, класс, в котором учится студент.
+// Метод displayInfo() - выводит информацию о студенте (имя, возраст и класс).
+
+class Student {
+  constructor(sName, sAge, sGrade) {
+    this.name = sName;
+    this.age = sAge;
+    this.grade = sGrade;
+  }
+
+  displayInfo() {
+    console.log(
+      `Name: ${this.name} \n Age: ${this.age} \n Grade: ${this.grade}`
+    );
+  }
+}
+
+const student1 = new Student("John Smith", 16, "10th grade");
+student1.displayInfo();
+const student2 = new Student("Jane Doe", 17, "11th grade");
+student2.displayInfo();
